e144a8894b1069234190f7417173e04d18fa7343
rfoster6 <32027942+rfoster6@users.noreply.github.com>
2017-12-22 07:41:39 -0500

    Initial commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeController : MonoBehaviour {
+	public int myGridX, myGridY;
+	public static int pusher1X, pusher1Y, pusher2X, pusher2Y;
+	public bool pusherCube1, pusherCube2;
+	public bool moveAllowed, moveUp, moveDown, moveRight, moveLeft;
+	GameController myGameController;
+	int myStartGridX, myStartGridY; 
+	public int myColorIndex;
+	float pushTime, lootTime;
+
+
+	// Use this for initialization
+	void Start () {
+		myGameController = GameObject.Find ("GameControllerObject").GetComponent<GameController> ();
+		pushTime = 1.5f;
+		if (pusherCube1 == false && pusherCube2 == false) {
+			CheckSpawnColor ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovePusherCubes ();
+
+		if (pusherCube1) {
+			pusher1X = myGridX;
+			pusher1Y = myGridY;
+		}
+
+		if (pusherCube2) {
+			pusher2X = myGridX;
+			pusher2Y = myGridY;
+		}
+
+		if (myGameController.resolutionPhaseActivated) {
+			StartCoroutine (ProcessResolutionPhase ());
+		}
+
+		if (moveAllowed) {
+			PushCubes ();
+		}
+			
+	}
+
+	void CheckSpawnColor () {
+		if (myGameController.cubeGrid [myGridX - 1, myGridY] != null && myGameController.cubeGrid [myGridX + 1, myGridY] != null) {
+			if (myColorIndex == myGameController.cubeGrid [myGridX - 1, myGridY].GetComponent <CubeController> ().myColorIndex
+				&& myColorIndex == myGameController.cubeGrid [myGridX + 1, myGridY].GetComponent <CubeController> ().myColorIndex) {
+				myColorIndex = Random.Range (0, myGameController.colors.Length);
+			}
+		}
+		if (myGameController.cubeGrid [myGridX, myGridY - 1] != null && myGameController.cubeGrid [myGridX, myGridY + 1] != null) {	
+			if (myColorIndex == myGameController.cubeGrid [myGridX, myGridY - 1].GetComponent <CubeController> ().myColorIndex
+			    && myColorIndex == myGameController.cubeGrid [myGridX, myGridY + 1].GetComponent <CubeController> ().myColorIndex) {
+				myColorIndex = Random.Range (0, myGameController.colors.Length);
+			}
+		}
+
+		gameObject.GetComponent <Renderer> ().material.color = myGameController.colors [myColorIndex];
+	}
+
+	void OnMouseDown () {
+		print (myGridX + ".." + myGridY);
+		if (myGameController.actionPhaseActivated) {
+			myGameController.cubeGrid [myGridX, myGridY] = null;
+			Destroy (gameObject);
+
+		}
+
+	}
+
+	public void MoveCounterClockwise () {
+
+		if (myGridY == 0) {
+			if (myGridX < myGameController.maxCubeSpawnX) {
+				myGridX++;
+			} else {
+				myGridX++;
+				myGridY++;
+			}
+		}
+		else if (myGridY == myGameController.playGridY - 1) {
+			if (myGridX > 1) {
+				myGridX--;
+			} else {
+				myGridX--;
+				myGridY--;
+			}
+		}
+		else if (myGridX == 0) {
+			if (myGridY > 1) {
+				myGridY--;
+			} else {
+				myGridX++;
+				myGridY--;
+			}
+		}
+		else if (myGridX == myGameController.playGridX - 1) {
+			if (myGridY < myGameController.maxCubeSpawnY) {
+				myGridY++;
+			} else {
+				myGridX--;
+				myGridY++;
+			}
+		}
+	}
+
+	public void MoveClockwise () {
+
+		if (myGridY == 0) {
+			if (myGridX > 1) {
+				myGridX--;
+			} else {
+				myGridX--;
+				myGridY++;
+			}
+		}
+		else if (myGridY == myGameController.playGridY - 1) {
+			if (myGridX < myGameController.maxCubeSpawnX) {
+				myGridX++;
+			} else {
+				myGridX++;
+				myGridY--;
+			}
+		}
+		else if (myGridX == 0) {
+			if (myGridY < myGameController.maxCubeSpawnY) {
+				myGridY++;
+				print (myGridY);
+			} else {
+				myGridX++;
+				myGridY++;
+			}
+		}
+		else if (myGridX == myGameController.playGridX - 1) {
+			if (myGridY > 0) {
+				myGridY--;
+			} else {
+				myGridX--;
+				myGridY--;
+			}
+		}
+	}
+
+	void MovePusherCubes () {
+		myStartGridX = myGridX;
+		myStartGridY = myGridY;
+
+		if (pusherCube1) {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				MoveClockwise ();
+				if (myGameController.cubeGrid [myGridX, myGridY] != null) {
+					MoveClockwise ();
+				}
+
+			}
+
+			else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				MoveCounterClockwise ();
+				if (myGameController.cubeGrid [myGridX, myGridY] != null) {
+					MoveCounterClockwise ();
+				}
+			}
+
+			pusher1X = myGridX;
+			pusher1Y = myGridY;
+		}
+
+		if (pusherCube2) {
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				MoveClockwise ();
+				if (myGameController.cubeGrid [myGridX, myGridY] != null) {
+					MoveClockwise ();
+				}
+			} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				MoveCounterClockwise ();
+				if (myGameController.cubeGrid [myGridX, myGridY] != null) {
+					MoveCounterClockwise ();
+				}
+			}
+
+			pusher2X = myGridX;
+			pusher2Y = myGridY;
+		}
+
+		myGameController.cubeGrid [myStartGridX, myStartGridY] = null;
+		myGameController.cubeGrid [myGridX, myGridY] = gameObject;
+		gameObject.transform.position = myGameController.gridPositions [myGridX, myGridY];
+
+
+	}
+
+	IEnumerator ProcessResolutionPhase () {
+		if (pusherCube1) {
+			moveAllowed = true;
+		}
+		yield return new WaitForSeconds (pushTime);
+
+		if (pusherCube2) {
+			moveAllowed = true;
+		}
+		yield return new WaitForSeconds (pushTime);
+		myGameController.resolutionPhaseActivated = false;
+		CollectLoot ();
+		yield return new WaitForSeconds (lootTime);
+
+	
+			
+	}
+
+	void ScoreCubes () {
+
+	}
+
+	void PushCubes () {
+		myStartGridX = myGridX;
+		myStartGridY = myGridY;
+
+		if (pusherCube1 || pusherCube2) {
+			if (myGridY == 0) {
+				moveUp = true;
+			} else if (myGridY == myGameController.playGridY - 1) {
+				moveDown = true;
+			} else if (myGridX == 0) {
+				moveRight = true;
+			} else if (myGridX == myGameController.playGridX - 1) {
+				moveLeft = true;
+			}
+		}
+
+		if (moveUp) {
+			if (myGameController.cubeGrid [myGridX, myGridY + 1] != null) {
+				myGameController.cubeGrid [myGridX, myGridY + 1].GetComponent<CubeController> ().moveUp = true;
+				myGameController.cubeGrid [myGridX, myGridY + 1].GetComponent<CubeController> ().moveAllowed = true;
+			}
+			myGridY++;
+			moveUp = false;
+		} else if (moveDown) {
+			if (myGameController.cubeGrid [myGridX, myGridY - 1] != null) {
+				myGameController.cubeGrid [myGridX, myGridY - 1].GetComponent<CubeController> ().moveDown = true;
+				myGameController.cubeGrid [myGridX, myGridY - 1].GetComponent<CubeController> ().moveAllowed = true;
+			}
+			myGridY--;
+			moveDown = false;
+		} else if (moveRight) {
+			if (myGameController.cubeGrid [myGridX + 1, myGridY] != null) {
+				myGameController.cubeGrid [myGridX + 1, myGridY].GetComponent<CubeController> ().moveRight = true;
+				myGameController.cubeGrid [myGridX + 1, myGridY].GetComponent<CubeController> ().moveAllowed = true;
+			} 
+			myGridX++;
+			moveRight = false;
+		} else if (moveLeft) {
+			if (myGameController.cubeGrid [myGridX - 1, myGridY] != null) {
+				myGameController.cubeGrid [myGridX - 1, myGridY].GetComponent<CubeController> ().moveLeft = true;
+				myGameController.cubeGrid [myGridX - 1, myGridY].GetComponent<CubeController> ().moveAllowed = true;
+			}
+			myGridX--;
+			moveLeft = false;
+		}
+
+
+		myGameController.cubeGrid [myStartGridX, myStartGridY] = null;
+		myGameController.cubeGrid [myGridX, myGridY] = gameObject;
+		StartCoroutine (ResolutionCubeMovement(myGameController.gridPositions [myGridX, myGridY]));
+		moveAllowed = false;
+		if (pusherCube1) {
+			pusherCube1 = false;
+		} 
+		if (pusherCube2) {
+			pusherCube2 = false;
+		}
+	}
+
+	IEnumerator ResolutionCubeMovement (Vector3 end){
+		float elapsedTime = 0;
+		Vector3 startingPos = myGameController.gridPositions [myStartGridX, myStartGridY];
+		while (elapsedTime < pushTime)
+			
+		{
+			gameObject.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / pushTime));
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		gameObject.transform.position = end;
+	}
+
+	void CollectLoot () {
+		if (myGridX == 0 || myGridX == myGameController.playGridX - 1 || myGridY == 0 || myGridY == myGameController.playGridY - 1) {
+			Destroy (gameObject);
+			myGameController.cubeGrid [myGridX, myGridY] = null;
+			myGameController.loot++;
+		}
+	}
+}
+ 
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public Text titleText, scoreText, turnsText, directionsText, actionButtonText;
+	public GameObject nonGamePlayScreenObject, gamePlayScreenObject;
+	public bool planningPhaseActivated, actionPhaseActivated, resolutionPhaseActivated, startScreenActivated, SummaryScreenActivated;
+	public int score, turnsLeft, maxTurns, loot;
+	public GameObject cubePreFab;
+	public GameObject[,] cubeGrid;
+	public Vector3[,] gridPositions;
+	public Color [] colors = new Color[6];
+	public Button playButton;
+	string blockShiftText = "Block Shift";
+	string startText = "Start";
+	string victoryText = "Victory!";
+	string playAgainText = "Play Again";
+	public int playGridX, playGridY, cubeGridX, cubeGridY, maxCubeSpawnX, maxCubeSpawnY;
+	int pusherCube1SpawnX, pusherCube1SpawnY, pusherCube2SpawnX, pusherCube2SpawnY;
+	float actionTimerLength;
+	//Vector3 [] textAndButtonPositions;
+
+
+	// Use this for initialization
+	void Start () {
+		startScreenActivated = true;
+		gamePlayScreenObject.SetActive (false);
+		actionTimerLength = 4f;
+		score = 0;
+		maxTurns = 15;
+		turnsLeft = maxTurns;
+		maxCubeSpawnX = 8;
+		maxCubeSpawnY = 5;
+		cubeGridX = maxCubeSpawnX + 2;
+		cubeGridY = maxCubeSpawnY + 2;
+		playGridX = cubeGridX;
+		playGridY = cubeGridY;
+		pusherCube1SpawnX = 0;
+		pusherCube1SpawnY = 1;
+		pusherCube2SpawnX = 1;
+		pusherCube2SpawnY = 0;
+		cubeGrid = new GameObject[cubeGridX, cubeGridY];
+		gridPositions = new Vector3[playGridX, playGridY];
+		colors [0] = Color.black;
+		colors [1] = Color.blue;
+		colors [2] = Color.green;
+		colors [3] = Color.red;
+		colors [4] = Color.yellow;
+		colors [5] = Color.white;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startScreenActivated) {
+			DisplayNonGameplayScreen (blockShiftText, startText);
+			startScreenActivated = false;
+		}
+		if (planningPhaseActivated) {
+			SetUpPlanningPhase ();
+			planningPhaseActivated = false;
+		}
+		if (actionPhaseActivated) {
+			RunActionTimer ();
+		}
+
+		if (SummaryScreenActivated) {
+			DisplayNonGameplayScreen (victoryText, playAgainText);
+			SummaryScreenActivated = false;
+		}
+	}
+
+	public void ProcessButtonClick () {
+		nonGamePlayScreenObject.SetActive (false);
+		gamePlayScreenObject.SetActive (true);
+		planningPhaseActivated = true;
+	}
+
+	void DisplayNonGameplayScreen (string myTitleText, string myButtonText) {
+		nonGamePlayScreenObject.SetActive (true);
+		titleText.text = myTitleText;
+		playButton.GetComponentInChildren <Text> ().text = myButtonText;
+
+	}
+
+
+
+	void SetUpPlanningPhase () {
+		//First time, set up grid
+		if (turnsLeft == maxTurns) {
+			CreateGrids ();
+		}
+		SpawnPusherCubes ();
+		directionsText.text = ("Press the Action! button when ready.");
+		actionButtonText.text = ("Action!");
+		turnsText.text = ("Turns:" + turnsLeft);
+		scoreText.text = ("Score:" + score);
+	}
+
+	void CreateGrids () {
+		print ("setting up grid");
+		for (int y = 0; y < playGridY; y++) {
+			for (int x = 0; x < playGridX; x++) {
+				gridPositions [x, y] = new Vector3 (x*2-15, y*2-8);
+				if (x > 0 && x <= maxCubeSpawnX && y > 0 && y <= maxCubeSpawnY) {
+					cubeGrid [x, y] = Instantiate (cubePreFab, gridPositions [x, y], Quaternion.identity);
+					cubeGrid [x, y].GetComponent<CubeController> ().myColorIndex = Random.Range (0, colors.Length);
+					cubeGrid [x, y].GetComponent<Renderer> ().material.color = colors [cubeGrid [x, y].GetComponent<CubeController> ().myColorIndex];
+					cubeGrid [x, y].GetComponent<CubeController> ().myGridX = x;
+					cubeGrid [x, y].GetComponent<CubeController> ().myGridY = y;
+
+				}
+
+			}
+		}
+
+	}
+
+	void SpawnPusherCubes () {
+		cubeGrid [pusherCube1SpawnX, pusherCube1SpawnY] = Instantiate (cubePreFab, gridPositions [pusherCube1SpawnX, pusherCube1SpawnY], Quaternion.identity);
+		cubeGrid [pusherCube1SpawnX, pusherCube1SpawnY].GetComponent<Renderer> ().material.color = colors [Random.Range (0, colors.Length)];
+		cubeGrid [pusherCube1SpawnX, pusherCube1SpawnY].GetComponent<CubeController> ().myGridX = pusherCube1SpawnX;
+		cubeGrid [pusherCube1SpawnX, pusherCube1SpawnY].GetComponent<CubeController> ().myGridY = pusherCube1SpawnY;
+		cubeGrid [pusherCube1SpawnX, pusherCube1SpawnY].GetComponent<CubeController> ().pusherCube1 = true;
+
+		cubeGrid [pusherCube2SpawnX, pusherCube2SpawnY] = Instantiate (cubePreFab, gridPositions [pusherCube2SpawnX, pusherCube2SpawnY], Quaternion.identity);
+		cubeGrid [pusherCube2SpawnX, pusherCube2SpawnY].GetComponent<Renderer> ().material.color = colors [Random.Range (0, colors.Length)];
+		cubeGrid [pusherCube2SpawnX, pusherCube2SpawnY].GetComponent<CubeController> ().myGridX = pusherCube2SpawnX;
+		cubeGrid [pusherCube2SpawnX, pusherCube2SpawnY].GetComponent<CubeController> ().myGridY = pusherCube2SpawnY;
+		cubeGrid [pusherCube2SpawnX, pusherCube2SpawnY].GetComponent<CubeController> ().pusherCube2 = true;
+	}
+		
+
+	public void StartActionPhase () {
+		actionPhaseActivated = true;
+		directionsText.text = ("You may click cubes to destroy them.");
+	}
+
+	void RunActionTimer () {
+		actionTimerLength -= Time.deltaTime;
+		actionButtonText.text = actionTimerLength.ToString ("00");
+
+		if (actionTimerLength <= 0f) {
+			actionPhaseActivated = false;
+			RunResolutionPhase ();
+		}
+
+	}
+
+	void RunResolutionPhase () {
+		resolutionPhaseActivated = true;
+		actionButtonText.text = (" ");
+		directionsText.text = (" ");
+		//scoring
+		//cubes fall
+		//new cubes
+		//repeat if needed
+	}
+		
+}
